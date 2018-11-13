ccd98f591f1969dd063ffcf3443b9ddaf9afbeb4
Patrik Meijer <patrik.meijer@hotmail.com>
2018-11-13 14:53:53 +0100

    ändrat formulär, nu autosum

--- script.js
+++ script.js
@@ -18,18 +18,39 @@ var game = {
     player = playerz[i],
 }
 
- */
+ 
 
 var sum = document.getElementById("sum");
-sum.addEventListener("click", function (event) {
-    let setSum= document.getElementById()
-    let aSumOnes = parseInt(document.getElementById("Player1_ones").value);
-    let aSumTwos = parseInt(document.getElementById("Player1_twos").value);
-    let aSumThrees = parseInt(document.getElementById("Player1_threes").value);
-    let aSumFours = parseInt(document.getElementById("Player1_fours").value);
-    let aSumFives = parseInt(document.getElementById("Player1_fives").value);
-    let aSumSix = parseInt(document.getElementById("Player1_six").value);
-    let sum= aSumOnes + aSumTwos +aSumThrees + aSumFours + aSumFives +aSumSix;
-    aSumOnes.value=sum;
-    console.log(sum);
-});
+sum.addEventListener("click", function (event)
+*/
+
+function checkNaN(player) {
+    if (document.getElementById(player).value === ""){
+    return 0;
+    }
+    else return document.getElementById(player).value;
+}
+    function sumPlayer1 (){
+    let setSum= document.getElementById("Player1_sum")
+    let aSumOnes = parseInt(checkNaN("Player1_ones"));
+    let aSumTwos = parseInt(checkNaN("Player1_twos"));
+    let aSumThrees = parseInt(checkNaN("Player1_threes"));
+    let aSumFours = parseInt(checkNaN("Player1_fours"));
+    let aSumFives = parseInt(checkNaN("Player1_fives"));
+    let aSumSix = parseInt(checkNaN("Player1_six"));
+    let sum = aSumOnes + aSumTwos +aSumThrees + aSumFours + aSumFives +aSumSix;
+    setSum.value = sum;
+}
+
+    var ones = document.getElementById("Player1_ones");
+    ones.addEventListener("change", sumPlayer1);
+    var twos = document.getElementById("Player1_twos");
+    twos.addEventListener("change", sumPlayer1);    
+    var threes = document.getElementById("Player1_threes");
+    threes.addEventListener("change", sumPlayer1);    
+    var fours = document.getElementById("Player1_fours");
+    fours.addEventListener("change", sumPlayer1);    
+    var fives = document.getElementById("Player1_fives");
+    fives.addEventListener("change", sumPlayer1);    
+    var six = document.getElementById("Player1_six");
+    six.addEventListener("change", sumPlayer1);
