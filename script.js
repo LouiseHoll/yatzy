2c9f220a43534045e25771688d18157d406bcd9e
LouiseHoll <louise.hollingworth@gmail.com>
2018-11-13 10:53:47 +0100

    Haaands

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+var playerz = [];
+
+var player = {
+    playerId: 123,
+    password: "StoraKatten1",
+    name: "Katten Kattzon",
+    nickname: "Yatzter",
+    email: "katten@katthub.com",
+    lastGame: 234,
+    bestGame: 324,
+}
+
+var gamez = [];
+
+var game = {
+    player = playerz[i],
+}
+
